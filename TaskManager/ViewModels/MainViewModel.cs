5edb8bac4ef9585eaeb7cc12220ed279de2a2176
Ludovic Belzile <ludovic.belzile@hotmail.fr>
2019-11-07 11:15:05 -0500

    Initial Commit

---
+++ TaskManager/ViewModels/MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using SimpleMvvmToolkit.Express;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using ToastNotifications;
+using ToastNotifications.Lifetime;
+using ToastNotifications.Messages;
+using ToastNotifications.Position;
+
+namespace TaskManager.ViewModels
+{
+    public class MainViewModel : ViewModelBase<MainViewModel>
+    {
+        private Notifier notifier;
+        private String message;
+
+        public ICommand ShowToastCommand => new DelegateCommand(ShowToast);
+
+        public MainViewModel()
+        {
+            notifier = new Notifier(cfg =>
+            {
+                cfg.PositionProvider = new PrimaryScreenPositionProvider(
+                    corner: Corner.BottomRight,
+                    offsetX: 10,
+                    offsetY: 10);
+
+                cfg.LifetimeSupervisor = new TimeAndCountBasedLifetimeSupervisor(
+                    notificationLifetime: TimeSpan.FromSeconds(3),
+                    maximumNotificationCount: MaximumNotificationCount.FromCount(5));
+
+                cfg.Dispatcher = Application.Current.Dispatcher;
+            });
+            message = "coucou";
+        }
+
+        public void ShowToast()
+        {
+            notifier.ShowInformation(message);
+        }
+    }
+}
---
+++ TaskManager/ViewModels/SystemTrayViewModel.cs
@@ -0,0 +1,35 @@
+﻿using SimpleMvvmToolkit.Express;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace TaskManager.ViewModels
+{
+    class SystemTrayViewModel : ViewModelBase<SystemTrayViewModel>
+    {
+        public ICommand ShowTMWindowCommand => new DelegateCommand(ShowTMWindow);
+        public ICommand QuitApplicationCommand => new DelegateCommand(QuitApplication);
+
+        bool opened = false;
+
+        private void ShowTMWindow()
+        {
+            if (!opened)
+            {
+                opened = true;
+                var window = new MainWindow();
+                window.Show();
+            }
+        }
+
+        private void QuitApplication()
+        {
+            opened = false;
+            Application.Current.Shutdown();
+        }
+    }
+}
