7024ea617e92edcf4a6b125c20804d15f7a8fac8
LudoB99 <ludovic.belzile@hotmail.fr>
2019-11-14 16:48:15 -0500

    EoD Push

--- TaskManager/ViewModels/MainViewModel.cs
+++ TaskManager/ViewModels/MainViewModel.cs
@@ -4,35 +4,62 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
+using System.Drawing;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Threading;
+using System.Threading;
 
 namespace TaskManager.ViewModels
 {
     public class MainViewModel : ViewModelBase<MainViewModel>
     {
         Toast toast;
-        Process[] proc; 
-        //public ICommand ShowToastCommand => new DelegateCommand(ShowToast);
+        List<Process> proc;
+        private Dispatcher dispatcher;
+        ObservableCollection<Proc> processList;
 
         public MainViewModel()
         {
-            GetAllProcesses(); 
+            dispatcher = Dispatcher.CurrentDispatcher;
+            processList = new ObservableCollection<Proc>();
+            proc = Process.GetProcesses().GroupBy(p => p.ProcessName).Select(group => group.First()).ToList();
+            Start();
         }
 
-        public void GetAllProcesses() 
+        public void Start() 
         {
-            proc = Process.GetProcesses().OrderBy(p => p.ProcessName).ToArray();
+            InstanciateProcesses();  // Start le thread, peut faire .wait() pour être sur que c'est fini. 
+        }
 
-            Console.WriteLine("System found {0} processes.", proc.Length);
-
-            foreach (Process p in proc) 
+        public void InstanciateProcesses()
+        { 
+            Icon Ico = null;
+            foreach (Process proc in proc)
             {
-                Console.WriteLine("Process: {0} ID: {1}", p.ProcessName, p.Id);
+                List<String> instancesNames = new PerformanceCounterCategory("Process")
+                    .GetInstanceNames().Where(instanceName => instanceName.StartsWith(proc.ProcessName)).ToList();
+                foreach (String instance in instancesNames)
+                {
+                    processList.Add(new Proc(instance, 0, 0, null));
+                }
             }
         }
+
+        public ObservableCollection<Proc> ProcessList
+        {
+            get { return processList;  }
+            set { processList = value; NotifyPropertyChanged("ProcessName"); }
+        }
+
+        public void ShowToast() {
+            Toast toast = new Toast();
+            toast.showToast("coucou"); 
+        }
+
+        //Ico = Icon.ExtractAssociatedIcon(proc[i].MainModule.FileName); // Va chercher l'icône du processus. --> Ça chie toujours...
     }
 }
   
---
+++ TaskManager/ViewModels/Proc.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.ViewModels
+{
+    public class Proc
+    {
+        public string Name { get; set; }
+        public double Cpu { get; set; }
+        public long Ram { get; set; }
+        public Icon Icon { get; set; }
+
+
+        public Proc(String name, double cpu, long ram, Icon Icon)
+        {
+            this.Name = name;
+            this.Cpu = cpu;
+            this.Ram = ram;
+            this.Icon = Icon; 
+        }
+
+        public String GetInfos()
+        {
+            return $"Process {Name} uses {Cpu}MB of CPU and {Ram}MB of RAM"; 
+        }
+    }
+}
