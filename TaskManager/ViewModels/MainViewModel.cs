56a37cb392cd9550bbbb70496f80799786a48d7b
LudoB99 <ludovic.belzile@hotmail.fr>
2019-11-14 20:08:04 -0500

    Affichage du nom des processus en threading.

--- TaskManager/ViewModels/MainViewModel.cs
+++ TaskManager/ViewModels/MainViewModel.cs
@@ -18,31 +18,33 @@ namespace TaskManager.ViewModels
     {
         Toast toast; 
         List<Process> Processes; // Liste de tous les processus actifs. 
-        private Dispatcher dispatcher; // Dispatcher, voir projet fait en classe. 
+        private Dispatcher Dispatch;
         ObservableCollection<Proc> processList; // Liste des processus dans le XAML.
 
         public MainViewModel()
         {
             Console.WriteLine("Démarrage");
-            dispatcher = Dispatcher.CurrentDispatcher; // Instancie le dispatcher qui gère la concurrence. 
+            Dispatch = Application.Current.Dispatcher; // Instancie le dispatcher qui gère la concurrence. 
             processList = new ObservableCollection<Proc>(); // Instancie la liste des processus dans le XAML.
-            Start();
+            Start(); 
         }
-
         public void Start() 
         {
-            Task.Run(UpdateAllProcesses).Wait();  // Update la liste, la boucle attend que l'update soit finie
-            Thread.Sleep(2000);
+            Thread Thread = new Thread (UpdateAllProcesses);
+            Thread.Start();
         }
 
         public void UpdateAllProcesses()
         {
-            Processes = Process.GetProcesses().GroupBy(p => p.ProcessName).Select(group => group.First()).ToList();
-            for (int i = 0; i < 10; ++i) 
+            while (true)
             {
-                List<String> ProcessInstancesNames = GetInstancesNames(Processes[i]);
-                Task.Run(() => PopulateList(ProcessInstancesNames)).Wait(); // Affiche les informations des processus dans le XAML.          
-            } 
+                Processes = Process.GetProcesses().GroupBy(p => p.ProcessName).Select(group => group.First()).ToList();
+                foreach (Process Process in Processes)
+                {
+                    List<String> ProcessInstancesNames = GetInstancesNames(Process);
+                    Task.Run(() => PopulateList(ProcessInstancesNames)).Wait(); // Affiche les informations des processus dans le XAML.
+                }
+            }
         }
 
         public void PopulateList(List <String> InstanceNames) 
@@ -50,23 +52,19 @@ namespace TaskManager.ViewModels
             foreach (String InstanceName in InstanceNames)
             {
                 Proc Process = GetProcessInfos(InstanceName);
-                Console.WriteLine("On update le process {0}", InstanceName);
-                dispatcher.Invoke(() => UpdateList(Process)); /*UpdateList(Process)*/  // Affiche les informations d'un processus dans le XAML. 
-                Console.WriteLine("Processus {0} updaté", InstanceName);
+                Dispatch.Invoke(() => UpdateList(Process)); /*UpdateList(Process)*/  // Affiche les informations d'un processus dans le XAML. 
             }
         }
         public void UpdateList(Proc NewProcess) 
         {
-            Console.WriteLine("On update dans la liste le process {0}", NewProcess.Name);
             Proc OldProcess = processList.FirstOrDefault(name => name.Name == NewProcess.Name); // Retoune le processus dans la liste. 
             if (OldProcess != null) 
             {
                 OldProcess = NewProcess;
-                Console.WriteLine("Le processus existait déjà, il y a encore {0} processus dans la liste.", processList.Count);
                 return; 
             }
             processList.Add(NewProcess); //Si le processus n'est pas dans la liste, on l'ajoute. 
-            Console.WriteLine("Le processus n'existait pas, il y a maintenant {0} processus dans la liste.", processList.Count);
+            Console.WriteLine("Nouveau processus: {0}", NewProcess.Name);
         }
 
         public Proc GetProcessInfos(String ProcessName) 
